2ff2acfd7ff0275fc43594fcd8cc0ebacab14efa
keydian <jmg.pinto@campus.fct.unl.pt>
2023-03-14 16:41:45 +0000

    Format Fixes again POG

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -1,11 +1,11 @@
 export const Formats: FormatList = [
 
 {
-	section: 'The Crew',
+	section: 'The Crew Draft League',
 	column: 3,
 },
 {
-	name: '[Gen 9] LC NatDex Draft',
+	name: '[Gen 9] NatDex LC',
 	mod: 'gen9',
 	ruleset: ['Little Cup', 'Standard NatDex'],
 	banlist: ['Dragon Rage', 'Sonic Boom'],
