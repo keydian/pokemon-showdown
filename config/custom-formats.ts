9980a826e7587640878172631d95a9885b9969cb
keydian <jmg.pinto@campus.fct.unl.pt>
2023-03-14 16:23:43 +0000

    Format Fix

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -6,9 +6,9 @@ export const Formats: FormatList = [
 },
 {
 	name: '[Gen 9] LC NatDex Draft',
-	mods: 'gen9',
+	mod: 'gen9',
 	ruleset: ['Little Cup', 'Standard NatDex'],
-	banlist: ['Dragon Rage', 'Sonic Boom'],
+	banlist: ['Unreleased', 'Dragon Rage', 'Sonic Boom'],
 
 },
 
