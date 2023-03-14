3370d7bfef6a99a616f54cd6eb448f77759c34af
keydian <49322025+keydian@users.noreply.github.com>
2023-03-14 15:09:26 +0000

    Custom Formats

---
+++ config/custom-formats.ts
@@ -0,0 +1,15 @@
+export const Formats: FormatList = [
+
+{
+	section: 'The Crew',
+	column: 3,
+},
+{
+	name: '[Gen 9] LC NatDex Draft',
+	mods: 'gen9',
+	ruleset: ['Little Cup', 'Standard NatDex'],
+	banlist: ['Dragon Rage', 'Sonic Boom'],
+
+},
+
+];
