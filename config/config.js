c36cc634cbc20f9d6d7e7efb43e437081f53342a
kdnhard <jmg.pinto@campus.fct.unl.pt>
2023-04-11 21:37:25 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -13,7 +13,7 @@ exports.port = 8000;
  *
  * @type {string}
  */
-exports.bindaddress = '0.0.0.0';
+exports.bindaddress = '51.77.194.198';
 
 /**
  * workers - the number of networking child processes to spawn
